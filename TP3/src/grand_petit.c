c6734db4400f1cbe94e04d4f5566af8427481b9d
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-07 15:17:26 +0200

    fini

--- TP3/src/grand_petit.c
+++ TP3/src/grand_petit.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
+#define N 100 //on définit la taille du tableau
+
 int main() {    
-    int tableau[100]; 
+    int tableau[N]; //création du tableau de taille 100
     int i;
-    for(i = 0; i < 100; i++) {
-        tableau[i] = i;
+    for(i = 0; i < N; i++) {
+        tableau[i] = i; // on remplit le tableau avec des valeurs allant de 0 à 99
     }
-    int grand = 0;
+    int grand = 0; //plus petit entier possible posiitf
     int petit = 32767; //plus grand entier possible
-    for(i = 0; i < 100; i++) {
-        if(tableau[i] > grand) {
-            grand = tableau[i];
+    for(i = 0; i < N; i++) { //on teste pour chaque valeur du tableau si elle est plus grande ou plus petite que les valeurs définies précédemment
+        if(tableau[i] > grand) { //si la valeur testée est plus grande que la plus grande valeur précédente
+            grand = tableau[i]; //on remplace cette dernière par la nouvelle plus grande valeur, jusqu'à trouver la plus grande du tableau
         }
-        if(tableau[i] < petit) {
-            petit = tableau[i];
+        if(tableau[i] < petit) { //si la valeur testée est plus petite que la plus petite valeur précédente
+            petit = tableau[i]; //on remplace cette dernière par la nouvelle plus petite valeur, jusqu'à trouver la plus petite du tableau
         }
     }
-    printf("plus grand entier du tableau: %d\n", grand);
+    //on affiche les deux valeurs limites trouvées
+    printf("plus grand entier du tableau: %d\n", grand); 
     printf("plus petit entier du tableau: %d\n", petit);
+    
     return(0);
 }
-            
-    
