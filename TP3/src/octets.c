407a50f082c55396a4615d51646057ba440364ac
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-07 18:39:57 +0200

    fini

--- TP3/src/octets.c
+++ TP3/src/octets.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    short s = 200;
-    char* sptr =(char *) &s;
-    int i = 200;
+    //on crée plusieurs variables de différents types
+    //on crée pour chaque variable un pointeur pointant leurs adresses (de type char car ce type ne prend qu'on octet ce qui est ce que l'on veut afficher)
+    short s = 150;
+    char* sptr =(char *)&s; 
+
+    int i = 98;
     char* iptr = (char *)&i;
+
     long l = -1234553L;
     char* lptr = (char *)&l;
+
     float f = 3.14;
-    char* fptr = (char *) &f;
+    char* fptr = (char *)&f;
+
     double d = 3.14E-12;
     char* dptr = (char *)&d;
+
     long double ld = 3.14E-22;
     char* ldptr = (char *)&ld;
     
+    //on affiche chaque octets des différentes variables (les bits de poids fort et faible sont inversés dans cette notation)
     printf("octets de short: \n");
     printf("%c\n",*sptr);
     printf("%c\n",*(sptr+1));
@@ -33,7 +41,6 @@ int main() {
     printf("%c\n", *(lptr+5));
     printf("%c\n", *(lptr+6));
     printf("%c\n", *(lptr+7));
-    printf("%c\n", *(lptr+8));
     
     printf("octets de float: \n");
     printf("%c\n", *fptr);
@@ -50,7 +57,6 @@ int main() {
     printf("%c\n", *(dptr+5));
     printf("%c\n", *(dptr+6));
     printf("%c\n", *(dptr+7));
-    printf("%c\n", *(dptr+8));
     
     printf("octets de long double: \n");
     printf("%c\n", *ldptr);
@@ -62,23 +68,20 @@ int main() {
     printf("%c\n", *(ldptr+6));
     printf("%c\n", *(ldptr+7));
     printf("%c\n", *(ldptr+8));
-    
+    printf("%c\n", *(ldptr+9));
+    printf("%c\n", *(ldptr+10));
+    printf("%c\n", *(ldptr+11));
+    printf("%c\n", *(ldptr+12));
+    printf("%c\n", *(ldptr+13));
+    printf("%c\n", *(ldptr+14));
+    printf("%c\n", *(ldptr+15));
     
     return(0);
 }
 
-
-
-//1 octets pour char
-//1 octets pour unsigned char
 //2 octets pour short
-//2 octets pour unsigned short
 //4 octets pour int
-//4 octets pour unsigned int
 //8 octets pour long int
-//8 octets pour unsigned long int
-//8 octets pour long long int
-//8 octets pour unsigned long long int
 //4 octets pour float
 //8 octets pour double
 //16 octets pour long double
