243b00a24bd0752f1cfb032a37821978e1a3c8f5
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-10 23:07:16 +0200

    Update bits.c

--- TP2/src/bits.c
+++ TP2/src/bits.c
@@ -1,11 +1,11 @@
-#include <stdio.h> // Bibliotèque utilisée
+#include <stdio.h> // Bibliothèque utilisée
 
 int main() {
-    int d =      0b00010000001100000001000000100000; // création d'une variable en binaire qui correespond à 20
-    int masque = 0b00010000000000000001000000000000; // création de notre masque à 4
-    int bits = d & masque; // création de notre variable bits qui doit respecter les 2 variables précédentes
+    int d =      0b00010000001100000001000000100000; // création de l'entier à tester 
+    int masque = 0b00010000000000000001000000000000; // création du masque qui va nous permettre de tester chaque bits de l'entier
+    int bits = d & masque; // création de la variable bits qui est un ET logique entre l'entier et le masque: si bits et le masque sont égaux, la condition est vérifiée
 
-    // Affiche 1 si la condition de l'ennoncé est respecté 
+    // Affiche 1 si la condition de l'enoncé est respectée
     if (bits == masque) {
         printf("%d\n", 1);
     }
@@ -16,5 +16,6 @@ int main() {
     return 0;
 }
 
-// Dans cet exercice, nous devions considérez un entier d et concevoir un code qui vérifie si les 4eme et 20eme bits de gauche sont 1 (en binaire). Si
-// les deux bits sont 1, il affiche 1 sinon 0. Pour cela, nous avions utilisé un masque 
+// Dans cet exercice, nous devions considérez un entier  et concevoir un code qui vérifie si les 4eme et 20eme bits de gauche de cet entier sont 1 (en binaire). Si
+// les deux bits sont 1, il affiche 1 sinon 0. Pour cela, nous avons utilisé un masque dont les 4eme et 20eme bits sont à 1 et le reste à 0. En effectuant un ET logique
+// entre l'entier et le masque, on obtient à nouveau le masque si les 4eme et 20eme bits de gauche de cet entier sont à 1.
