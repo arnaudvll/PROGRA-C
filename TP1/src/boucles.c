d4f2ad12f22704a2f0a2c4b772cbfb70843d6b1c
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-11 00:50:34 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,10 +1,10 @@
-#include <stdio.h> // Bibliotèque utilisée
+#include <stdio.h> // Bibliothèque utilisée
 
 int main() {
-    int compter =  100;
+    int compter =  10; // nombre de lignes que l'on veut afficher
     int ligne;
     int colonne;
-    for(ligne = 1; ligne < compter+1; ligne++) {
+    for(ligne = 1; ligne < compter+1; ligne++) { // on parcourt chaque ligne et on affiche en fonction de où l'on est
         if(ligne == 1) {
             printf("*\n"); // On s'occupe de la première ligne ici donc on affiche 1 "*" puis on va à la ligne suivante
         }
@@ -14,20 +14,20 @@ int main() {
         }
         
         else if(ligne == compter) {
-            for(; compter > 0; compter--){ // On s'occupe de la dernière ligne ici donc on affiche "compter-1" "*" puis on va à la ligne suivante
+            for(; compter > 0; compter--){ // On s'occupe de la dernière ligne ici donc on affiche "compter" "*" puis on va à la ligne suivante
                 printf("*");
             }   
         }
-        else {
-            printf("*");
+        else { // si on est dans aucun de ces cas
+            printf("*"); // on affiche 1 "*"
             for(colonne = 2; colonne < ligne; colonne++) {
-                printf("#"); // On affiche des "#" pour toutes les autres lignes et on affiche des "*" aux extrémités
+                printf("#"); // On affiche des "#" sur le reste de la ligne, sauf à la dernière position
             }
-            printf("*\n");
+            printf("*\n"); // on affiche 1 "*" en fin de ligne
         }    
     }
     printf("\n");
     return 0;
 }
-// Dans cet execrice, nous Écrivez un programme *boucles.c* qui utilise `for`, # et * et qui affiche un triangle rectangle. 
-// La taille du triangle est dependent de la valeur de la variable `compter` 
+// Dans cet exercice, nous avons fait un programme créant un triangle rectangle de "*", sauf à "l'intérieur" du triangle où on met des "#" 
+// La taille du triangle dépend de la valeur de la variable `compter` 
