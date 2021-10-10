54dc58f9001a5ee482c5eeab3d49db69e074637b
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-11 00:07:58 +0200

    Update etudiant.c

--- TP2/src/etudiant.c
+++ TP2/src/etudiant.c
@@ -1,20 +1,17 @@
-#include <stdio.h> // Bibliotèque utilisée
+#include <stdio.h> // Bibliothèque utilisée
 
 int main() {
 
-    // Pour la création de chaque liste, on utilise la fonction Char ( chaine de caractère )
-    char nom[][3] = {"A1", "A2", "A3", "A4", "A5"}; //création liste des noms 
-    char prenom[][15] = {"Arnaud", "Bob", "Charles", "Denis", "Esteban"}; //création liste des prénoms
-    char adresse[][15] = {"1 rue a", "2 rue b", "3 rue c", "4 rue d", "5 rue e"}; //création liste des adresses
-    char notes_progc[][3] = {"20", "14", "16", "2", "18"}; //création liste des notes 1
-    char notes_syst[][3] = {"20", "6", "17", "8", "12"}; //création liste des notes 2
+    char nom[][3] = {"A1", "A2", "A3", "A4", "A5"}; // création du tableau des noms (de taille 3 maximum)
+    char prenom[][15] = {"Arnaud", "Bob", "Charles", "Denis", "Esteban"}; // création du tableau des prénoms (de taille 15 maximum)
+    char adresse[][15] = {"1 rue a", "2 rue b", "3 rue c", "4 rue d", "5 rue e"}; // création du tableau des adresses (de taille 15 maximum))
+    char notes_progc[][3] = {"20", "14", "16", "2", "18"}; // création du tableau des notes de programmation en c (de taille 3 maximum)
+    char notes_syst[][3] = {"20", "6", "17", "8", "12"}; //création du tableau des notes de système d'exploitation (de taille 3 maximum)
+
     int i;
-    // Affichage de chaque donnés par étudiant, utilisation d'une boucle for, les i correspondent à 1 étudiant
-    // De 0 à 4
+    // Affichage  des données de chaque étudiant, utilisation d'une boucle for, chaque i correspond à 1 étudiant
     for (i = 0; i < 5; i++) {  
-        printf("étudiant %d: %s, %s\n adresse: %s\n note en programmation en c: %s: \n note en système d'exploitation: %s\n", i+1, nom[i], prenom[i], adresse[i], notes_progc[i], notes_syst[i]);
+        printf("étudiant %d: %s, %s\n adresse: %s\n note en programmation en c: %s \n note en système d'exploitation: %s\n", i+1, nom[i], prenom[i], adresse[i], notes_progc[i], notes_syst[i]);
     }
     return 0;
 }
- // Dans cet exercice, nous avons créé 5 listes de chaine de caratère et créer une boucle for qui permet d'afficher
- // chaque donné pour chaque étudiant
