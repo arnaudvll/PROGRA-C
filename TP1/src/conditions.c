f416bc2384270458d2454dbb54b3043576f6d18f
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-11 14:15:22 +0200

    Update conditions.c

--- TP1/src/conditions.c
+++ TP1/src/conditions.c
@@ -1,11 +1,11 @@
-#include <stdio.h> // Bibliotèque utilisée
+#include <stdio.h> // Bibliothèque utilisée
 
 int main() {
     int nb = 1000;
 
     // Première condition, notre variable nb doit être divisible par 2 et 15
     while (nb > 0) {
-        if ((nb % 2 == 0) && (nb % 15 == 0)) { // && correspond à l'opérateur logique ET
+        if ((nb % 2 == 0) && (nb % 15 == 0)) { // on vérifie les 2 conditions simultanément grâce au &&
             printf("%d est divisible par 2 et 15\n", nb);
         }
         nb--;
@@ -14,7 +14,7 @@ int main() {
     // Deuxième condition, notre variable nb doit être divisible par 103 ou 107
     nb = 1000;
     while (nb > 0) {
-        if ((nb % 103 == 0) || (nb % 107 == 0)) { // || correspond à l'opérateur logique OU
+        if ((nb % 103 == 0) || (nb % 107 == 0)) { // on vérifie l'une ou l'autre des conditions grâce au ||
             printf("%d est divisible par 103 ou 107\n", nb);
         }
         nb--;
@@ -23,8 +23,8 @@ int main() {
     // Troisième condition, notre variable nb doit être divisible par 7 ou 5 mais pas par 3
     nb = 1000;
     while (nb > 0) {
-        if ((nb % 7 == 0) || (nb % 5 == 0)) {
-            if (nb % 3 != 0) {
+        if ((nb % 7 == 0) || (nb % 5 == 0)) { // soit divisible par 7, soit par 5
+            if (nb % 3 != 0) { // non divisible par 3
                 printf("%d est divisble par 7 ou 5 mais pas par 3\n",nb);
             }
         }
