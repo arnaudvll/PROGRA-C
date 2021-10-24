4a671b05a56649332fba0af54b689d96b2e3fef8
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-24 19:01:39 +0200

    Update etudiant_bd.c

--- TP4/src/etudiant_bd.c
+++ TP4/src/etudiant_bd.c
@@ -4,61 +4,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include "etudiant_bd.h"
 
-int ecrire_dans_fichier(char *nom_de_fichier, char *message);
-int clear(void);
-
-int main() {
-    char nom_de_fichier[] = "etudiant.txt";
-    char nom[20];
-    char prenom[20];
-    char adresse[40];
-    char ville[20];
-    char note_c[3];
-    char note_sys[3];
-    
-    for (int i = 0; i < 5; i++) {
-        printf("Etudiant %d ", i);
-        printf("Nom: ");
-        scanf("%s", nom);
-        clear();
-        
-        printf("Prenom: ");
-        scanf("%s", prenom);
-        clear();
-        
-        printf("adresse: ");
-        scanf("%s", adresse);
-        clear();
-        
-        printf("ville: ");
-        scanf("%s", ville);
-        clear();
-        
-        printf("note_c: ");
-        scanf("%s", note_c);
-        clear();
-        
-        printf("note_sys: ");
-        scanf("%s", note_sys);
-        clear();
-        
-        ecrire_dans_fichier(nom_de_fichier, nom);
-        ecrire_dans_fichier(nom_de_fichier, ", ");
-        ecrire_dans_fichier(nom_de_fichier, prenom);
-        ecrire_dans_fichier(nom_de_fichier, ", ");
-        ecrire_dans_fichier(nom_de_fichier, adresse);
-        ecrire_dans_fichier(nom_de_fichier, ", ");
-        ecrire_dans_fichier(nom_de_fichier, ville);
-        ecrire_dans_fichier(nom_de_fichier, ", ");
-        ecrire_dans_fichier(nom_de_fichier, note_c);
-        ecrire_dans_fichier(nom_de_fichier, ", ");
-        ecrire_dans_fichier(nom_de_fichier, note_sys);
-        ecrire_dans_fichier(nom_de_fichier, "\n");
-        
-    }
-    return(0);
-}
 
 int ecrire_dans_fichier(char *nom_de_fichier, char *message) {
     int fd, count, size;
