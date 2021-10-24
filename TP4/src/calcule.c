2db1f8e989052a7a1a7c8789ae7cc4d8c5844bc4
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-24 19:03:47 +0200

    Update calcule.c

--- TP4/src/calcule.c
+++ TP4/src/calcule.c
@@ -4,49 +4,6 @@
 
 //https://stackoverflow.com/questions/7898215/how-to-clear-input-buffer-in-c a mettre en SOURCE
 
-int main(int argc, char* argv[]) { 
-    //le premier terme de argv correspond à l'éxécution de la fonction dans le terminal
-    char op = argv[1][0]; //on récupère le deuxième argument, soit l'opérateur (argv[1] = "opérateur \0" donc on prend argv[1][0]
-    int num1 = atoi(argv[2]); //on récupère le troisième argument, soit le 1er nombre
-    int num2 = atoi(argv[3]); //on récupère le quatrième argument, soit le 1er nombre
-    
-    //le code suivant est comme dans les fichiers 'opérateur' précédents
-    switch (op) {
-        case '+' : 
-            somme(num1, num2);
-            break;
-
-        case '-' : 
-            difference(num1, num2);
-            break;
-            
-        case '*' :
-            produit(num1, num2);
-            break;
-            
-        case '/' :
-            quotient(num1, num2);
-            break;
-            
-        case '%' :
-            modulo(num1, num2);
-            break;
-            
-        case '&' :
-            et(num1, num2);
-            break;
-            
-        case '|' :
-            ou(num1, num2);
-            break;
-            
-        case '~':
-            negation(num1);
-            break;
-    }
-    return 0;
-}
-
 int somme(int num1, int num2) {
     printf("addition %d\n",num1+num2);
     return 0;
