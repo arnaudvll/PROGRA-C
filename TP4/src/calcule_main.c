d9f8f148770cad111c7d9bd908d1aadf6dea6461
arnaudvll <72150786+arnaudvll@users.noreply.github.com>
2021-10-24 19:05:24 +0200

    Update calcule_main.c

--- TP4/src/calcule_main.c
+++ TP4/src/calcule_main.c
@@ -1,3 +1,7 @@
+#include <stdio.h> // Bibliothèque utilisée
+#include <stdlib.h>
+#include "operator.h"
+
 int main(int argc, char* argv[]) { 
     //le premier terme de argv correspond à l'éxécution de la fonction dans le terminal
     char op = argv[1][0]; //on récupère le deuxième argument, soit l'opérateur (argv[1] = "opérateur \0" donc on prend argv[1][0]
